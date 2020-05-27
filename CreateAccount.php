0c00cb1f7bad8271c139851246c3a81ea7b3f20c
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-05-27 11:27:15 +0530

    Adarsha | Adds password validation & removes forbidden phrase

--- CreateAccount.php
+++ CreateAccount.php
@@ -4,10 +4,6 @@ include 'Credentials.php';
 include 'Protection.php';
 include 'EncryptionKeys.php';
 
-foreach($_POST as $element)
-{
-    checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
-}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,12 +16,16 @@ if ($conn->connect_error) {
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
 $emailId=$conn->real_escape_string($_POST["emailId"]);
+$password=$conn->real_escape_string($_POST["password"]);
 
 $key_name="postAuthKey";
 
 $response=array();
 $response['success']=false;
 $response['validAuth']=false;
+$response['validEmail']=false;
+$response['validPassword']=false;
+
 
 $stmt=$conn->prepare("SELECT value FROM Auth_Keys WHERE name =?");
 $stmt->bind_param("s", $key_name);
@@ -37,9 +37,20 @@ if($stmt->fetch() && $postAuthKey1==$postAuthKey2)
     $stmt->close();
     $response['validAuth']=true;
 	
-	if(filter_var($emailId, FILTER_VALIDATE_EMAIL))
+	if(filter_var($emailId, FILTER_VALIDATE_EMAIL))		//email validation
 	{
-		$response['success']=true;
+		$response['validEmail']=true;
+		
+		$uppercase=preg_match('@[A-Z]@', $password);	//password validation
+		$lowercase=preg_match('@[a-z]@', $password);
+		$number=preg_match('@[0-9]@', $password);
+		$specialChars=preg_match('@[^\w]@', $password);
+		
+		if($uppercase && $lowercase && $number && $specialChars && strlen($password) >= 8 && strlen($password) <= 20)
+		{
+			$response['validPassword']=true;
+			$response['success']=true;
+		}
 	}
 }
 
