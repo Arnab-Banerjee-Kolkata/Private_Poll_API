66283a8abe4127af83fb28d5413f3187340a8874
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-05-27 12:01:12 +0530

    Adarsha | Changes OTP to password based login

--- AccountLogin.php
+++ AccountLogin.php
@@ -4,10 +4,6 @@ include 'Credentials.php';
 include 'Protection.php';
 include 'EncryptionKeys.php';
 
-foreach($_POST as $element)
-{
-    checkForbiddenPhrase($INTERNAL_AUTH_KEY, $element);
-}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,7 +16,7 @@ if ($conn->connect_error) {
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
 $emailId=$conn->real_escape_string($_POST["emailId"]);
-$otp=$conn->real_escape_string($_POST["otp"]);
+$password=$conn->real_escape_string($_POST["password"]);
 
 
 $key_name="postAuthKey";
@@ -30,7 +26,7 @@ $response=array();
 $response['success']=false;
 $response['validAuth']=false;
 $response['validEmail']=false;
-$response['validOtp']=false;
+$response['validPassword']=false;
 
 $stmt3=$conn->prepare("SELECT value FROM Auth_Keys WHERE name =?");
 $stmt3->bind_param("s", $key_name);
@@ -44,10 +40,10 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
     $stmt3->close();
     $response['validAuth']=true;
     
-    $stmt=$conn->prepare("SELECT COUNT(email_id), otp FROM Account WHERE email_id=? AND status=0");
+    $stmt=$conn->prepare("SELECT COUNT(email_id), password FROM Account WHERE email_id=? AND status=0");
     $stmt->bind_param("s", $emailId);
     $stmt->execute();
-    $stmt->bind_result($count, $realOtp);
+    $stmt->bind_result($count, $password2);
     $stmt->fetch();
     $stmt->close();
 
@@ -56,11 +52,9 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
         $count=-1;
         $response['validEmail']=true;
 
-        $otp=encrypt($INTERNAL_AUTH_KEY, $otp, $keySet);
-
-        if($otp==$realOtp)
+        if($password==$password2)
         {
-            $response['validOtp']=true;
+            $response['validPassword']=true;
 
             $stmt=$conn->prepare("UPDATE Account SET status=1 WHERE email_id=?");
             $stmt->bind_param("s",$emailId);
@@ -70,20 +64,10 @@ if($stmt3->fetch() && $postAuthKey1==$postAuthKey2)
 
             $response['success']=true;
         }
-        
-        
-        $otp1=mt_rand(1000, mt_rand(1001,9999));        
-        $otp1=encrypt($INTERNAL_AUTH_KEY, $otp1, $keySet);
-    
-		$stmt3=$conn->prepare("UPDATE Account SET otp=? WHERE email_id=?");
-		$stmt3->bind_param("ss",$otp1,$emailId);
-		$stmt3->execute();
-		$stmt3->close();
     }
 }
 $conn->close();
 
 echo json_encode($response);
-
 
 ?>
