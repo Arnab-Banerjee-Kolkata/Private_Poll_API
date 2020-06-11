eacfe4acdf7908a62a901534dc01bea7375dbd51
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-06-11 16:06:41 +0530

    Adarsha | Only Voter and Creator can see results

--- DisplayResult.php
+++ DisplayResult.php
@@ -16,6 +16,7 @@ if ($conn->connect_error) {
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
 $pollId=$conn->real_escape_string($_POST["pollId"]);
+$emailId=$conn->real_escape_string($_POST["emailId"]);
 
 $key_name="postAuthKey";
 
@@ -23,6 +24,7 @@ $response=array();
 $response['success']=false;
 $response['validAuth']=false;
 $response['validPoll']=false;
+$response['validUser']=false;
 
 
 $stmt=$conn->prepare("SELECT value FROM Auth_Keys WHERE name =?");
@@ -35,10 +37,10 @@ if($stmt->fetch() && $postAuthKey1==$postAuthKey2)
     $stmt->close();
     $response['validAuth']=true;
 	
-	$stmt2=$conn->prepare("SELECT COUNT(id) FROM Poll WHERE id=? AND status=2");
+	$stmt2=$conn->prepare("SELECT COUNT(id),account_id FROM Poll WHERE id=? AND status=2");
 	$stmt2->bind_param("d",$pollId);
 	$stmt2->execute();
-	$stmt2->bind_result($count);
+	$stmt2->bind_result($count,$creator);
 	
 	if($stmt2->fetch() && $count==1)
 	{
@@ -46,26 +48,40 @@ if($stmt->fetch() && $postAuthKey1==$postAuthKey2)
 		$stmt2->close();
 		$response['validPoll']=true;
 		
-		$stmt3=$conn->prepare("SELECT option_name,vote_count FROM Polling_Options WHERE poll_id=?");
-		$stmt3->bind_param("d",$pollId);
-		$stmt3->execute();
-		$stmt3->bind_result($optionName,$voteCount);
+		$stmt4=$conn->prepare("SELECT email_id FROM Voted_List WHERE poll_id=?");
+		$stmt4->bind_param("d",$pollId);
+		$stmt4->execute();
+		$stmt4->bind_result($voter);
+		$stmt4->fetch();
 		
-		$results=array();
-		
-		while($stmt3->fetch())
+		if($emailId==$creator || $emailId==$voter)
 		{
-			$options=array();
+			$stmt4->close();
+			$response['validUser']=true;
 			
-			$options['optionName']=$optionName;
-			$options['voteCount']=$voteCount;
+			$stmt3=$conn->prepare("SELECT option_name,vote_count FROM Polling_Options WHERE poll_id=?");
+			$stmt3->bind_param("d",$pollId);
+			$stmt3->execute();
+			$stmt3->bind_result($optionName,$voteCount);
 			
-			array_push($results,$options);
+			$results=array();
+			
+			while($stmt3->fetch())
+			{
+				$options=array();
+				
+				$options['optionName']=$optionName;
+				$options['voteCount']=$voteCount;
+				
+				array_push($results,$options);
+			}
+			$stmt3->close();
+			
+			$response['results']=$results;
+			$response['success']=true;
 		}
-		
-		$response['results']=$results;
-		$response['success']=true;
-		
+		else
+			$stmt4->close();
 	}
 	else
 		$stmt2->close();
