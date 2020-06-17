6c23e3328dc024fd61ac898530a5c1f666c134d7
Adarsha Shaw <62458669+adarshashaw@users.noreply.github.com>
2020-06-17 12:03:46 +0530

    Adarsha | Adds email validation for already voted

--- JoinElection.php
+++ JoinElection.php
@@ -16,6 +16,7 @@ if ($conn->connect_error) {
 
 $postAuthKey1=$conn->real_escape_string($_POST["postAuthKey"]);
 $pollId=$conn->real_escape_string($_POST["pollId"]);
+$emailId=$conn->real_escape_string($_POST["emailId"]);
 $entryCode=$conn->real_escape_string($_POST["entryCode"]);
 
 $key_name="postAuthKey";
@@ -24,6 +25,7 @@ $response=array();
 $response['success']=false;
 $response['validAuth']=false;
 $response['validPoll']=false;
+$response['validEmail']=false;
 $response['validEntryCode']=false;
 
 $stmt=$conn->prepare("SELECT value FROM Auth_Keys WHERE name =?");
@@ -46,32 +48,46 @@ if($stmt->fetch() && $postAuthKey1==$postAuthKey2)
 		$count=-1;
 		$stmt2->close();
 		$response['validPoll']=true;
+
+		$stmt4=$conn->prepare("SELECT COUNT(email_id) FROM Voted_List WHERE poll_id=? AND email_id=?");
+		$stmt4->bind_param("ds",$pollId,$emailId);
+		$stmt4->execute();
+		$stmt4->bind_result($count2);
 		
-		if($entryCode==$entryCode2)
+		if($stmt4->fetch() && $count2==0)
 		{
-			$response['validEntryCode']=true;
+			$count2=-1;
+			$stmt4->close();
+			$response['validEmail']=true;
 			
-			$stmt3=$conn->prepare("SELECT option_id,option_name FROM Polling_Options WHERE poll_id=?");
-			$stmt3->bind_param("d",$pollId);
-			$stmt3->execute();
-			$stmt3->bind_result($optionId,$optionName);
-			
-			$votingPanel=array();
-			
-			while($stmt3->fetch())
+			if($entryCode==$entryCode2)
 			{
-				$options=array();
+				$response['validEntryCode']=true;
 				
-				$options['optionName']=$optionName;
-				$options['voteCount']=$voteCount;
+				$stmt3=$conn->prepare("SELECT option_id,option_name FROM Polling_Options WHERE poll_id=?");
+				$stmt3->bind_param("d",$pollId);
+				$stmt3->execute();
+				$stmt3->bind_result($optionId,$optionName);
 				
-				array_push($votingPanel,$options);
+				$votingPanel=array();
+				
+				while($stmt3->fetch())
+				{
+					$options=array();
+					
+					$options['optionId']=$optionId;
+					$options['optionName']=$optionName;
+					
+					array_push($votingPanel,$options);
+				}
+				$stmt3->close();			
+				
+				$response['votingPanel']=$votingPanel;
+				$response['success']=true;
 			}
-			$stmt3->close();			
-			
-			$response['votingPanel']=$votingPanel;
-			$response['success']=true;
 		}
+		else
+			$stmt4->close();
 	}
 	else
 		$stmt2->close();
